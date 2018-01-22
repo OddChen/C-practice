b91e5739dbad18f4d7ba923ec64dfd7f34079072
陈星宇 <oddchenxy@foxmail.com>
2018-01-22 22:19:54 +0800

    Update plane.c
    实现敌机移动  添加分数统计

--- plane.c
+++ plane.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<windows.h>
 #include<conio.h>//可以从键盘获取输入
 #pragma warning(disable:4996)
 /*
- *可改进方向：实习移动的敌机，我们的飞机发射子弹击中他
+ *可改进方向：实现移动的敌机，我们的飞机发射子弹击中他
  *显示得分 敌机撞击我们后，判断并提示游戏失败
  *
  */
@@ -14,18 +15,40 @@ int main(){
 	char input;
 	int isFire = 0;
 
-	int ny = 5;//靶子，放在第一行，ny列
 	int isKilled = 0;
-
+	int score=0;
+	//关于敌机的一些定义
+	int dx = 5;
+	int dy = 10;
+	int velocity_dx = 1;
+	int velocity_dy = 1;
+	int left = 0;
+	int right = 100;
+	int top = 0;
+	int bottom = 30;
+	
 	while (1){
 		system("cls");//清屏
-
-		//输出靶子
+		//分数统计
+		printf("当前分数：%d\n", score);
+		
+		//运动的飞机
 		if (!isKilled){
-			for (j = 0; j < ny; j++){
+			//dx = dx + velocity_dx;
+			//dy = dy + velocity_dy;
+
+			for (i = 0; i<dx; i++)
+				printf("\n");
+			for (j = 0; j<dy; j++)
 				printf(" ");
-			}
-			printf("+\n");		
+
+			printf("*\n");  // 敌机
+			//Sleep(50);  // 等待若干毫秒
+
+			//if ((dx == top) || (dx == bottom))
+			//	velocity_dx = -velocity_dx;
+			//if ((dy == left) || (dy == right))
+			//	velocity_dy = -velocity_dy;		
 		}
 
 		//判断是否发射激光并输出
@@ -41,7 +64,7 @@ int main(){
 				}
 				printf("  |\n");
 			}
-			if (y + 2 == ny){
+			if (y + 2 == dy){
 				isKilled = 1;
 			}
 			isFire = 0;
